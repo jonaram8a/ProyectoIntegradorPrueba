8c932a30c7185fa08bc4339e98d840bbccceab2d
daniel <danieiniguez314@gmail.com>
2026-02-23 22:24:46 -0600

    Modificacion de modelo para rest

--- ChatApp/src/java/org/integracode/chatapp/model/Reciclaje.java
+++ ChatApp/src/java/org/integracode/chatapp/model/Reciclaje.java
@@ -5,28 +5,19 @@ public class Reciclaje {
     private String material;
     private double cantidad;
     private double puntos;
+    private String mensaje;
 
-    public String getMaterial() {
-        return material;
-    }
+    public Reciclaje() {}
 
-    public void setMaterial(String material) {
-        this.material = material;
-    }
+    public String getMaterial() { return material; }
+    public void setMaterial(String material) { this.material = material; }
 
-    public double getCantidad() {
-        return cantidad;
-    }
+    public double getCantidad() { return cantidad; }
+    public void setCantidad(double cantidad) { this.cantidad = cantidad; }
 
-    public void setCantidad(double cantidad) {
-        this.cantidad = cantidad;
-    }
+    public double getPuntos() { return puntos; }
+    public void setPuntos(double puntos) { this.puntos = puntos; }
 
-    public double getPuntos() {
-        return puntos;
-    }
-
-    public void setPuntos(double puntos) {
-        this.puntos = puntos;
-    }
+    public String getMensaje() { return mensaje; }
+    public void setMensaje(String mensaje) { this.mensaje = mensaje; }
 }
